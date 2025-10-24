1bd7eb3f0bfbf88a712ffad87b7c896efd2c01d3
hilmizul <netspytux@gmail.com>
2025-10-24 22:08:52 +0700

    more small and remove space bottom

--- components/NavFixedBottom.vue
+++ components/NavFixedBottom.vue
@@ -1,28 +1,28 @@
 <template>
-  <nav class="navbar navbar-expand fixed-bottom bg-white border-top border-3 border-dark p-0 pb-3">
+  <nav class="navbar navbar-expand fixed-bottom bg-white border-top border-3 border-dark p-0">
     <div class="container-fluid justify-content-around">
       <ul class="navbar-nav text-center fw-bold">
         <li class="nav-item">
           <nuxt-link class="nav-link" to="/" :activeClass="activeClass">
-            <i class="bi bi-house-door fs-4"></i> <br>
+            <i class="bi bi-house-door fs-5"></i> <br>
             <span class="smallest">Home</span>
           </nuxt-link>
         </li>
         <li class="nav-item mx-4">
           <nuxt-link class="nav-link" to="/elemen">
-            <i class="bi bi-journal-bookmark-fill fs-4"></i> <br>
+            <i class="bi bi-journal-bookmark-fill fs-5"></i> <br>
             <span class="smallest">Elemen</span>
           </nuxt-link>
         </li>
         <li class="nav-item me-4">
           <nuxt-link class="nav-link" to="/jurnal">
-            <i class="bi bi-journals fs-4"></i> <br>
+            <i class="bi bi-journals fs-5"></i> <br>
             <span class="smallest">Jurnal</span>
           </nuxt-link>
         </li>
         <li class="nav-item">
           <nuxt-link class="nav-link" href="/nilai">
-            <i class="bi bi-patch-check fs-4"></i> <br>
+            <i class="bi bi-patch-check fs-5"></i> <br>
             <span class="smallest">Nilai</span>
           </nuxt-link>
         </li>
