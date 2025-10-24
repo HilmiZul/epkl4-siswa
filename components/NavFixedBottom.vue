d06e821013bc27700877559d6a506a4a73b7a11c
hilmizul <netspytux@gmail.com>
2025-10-24 21:52:53 +0700

    create navbar bottom when open in smartphone

---
+++ components/NavFixedBottom.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar navbar-expand fixed-bottom bg-white border-top border-3 border-dark p-0 pb-3">
+    <div class="container-fluid justify-content-around">
+      <ul class="navbar-nav text-center fw-bold">
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="/" :activeClass="activeClass">
+            <i class="bi bi-house-door fs-4"></i> <br>
+            <span class="smallest">Home</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item mx-4">
+          <nuxt-link class="nav-link" to="/elemen">
+            <i class="bi bi-journal-bookmark-fill fs-4"></i> <br>
+            <span class="smallest">Elemen</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item me-4">
+          <nuxt-link class="nav-link" to="/jurnal">
+            <i class="bi bi-journals fs-4"></i> <br>
+            <span class="smallest">Jurnal</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" href="/nilai">
+            <i class="bi bi-patch-check fs-4"></i> <br>
+            <span class="smallest">Nilai</span>
+          </nuxt-link>
+        </li>
+        <!-- <li class="nav-item">
+          <nuxt-link class="nav-link" href="/ubah-password">
+            <i class="bi bi-key fs-4"></i> <br>
+            <span class="smallest">Password</span>
+          </nuxt-link>
+        </li> -->
+      </ul>
+    </div>
+  </nav>
+</template>
+
+
+<script setup>
+let activeClass = ref('list-group-item-active')
+</script>
