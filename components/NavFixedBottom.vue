0ab2379a2fa62e72aa2e7e7356d7cb831305eab8
hilmizul <netspytux@gmail.com>
2025-10-25 07:36:31 +0700

    add new item nav and rearrange

--- components/NavFixedBottom.vue
+++ components/NavFixedBottom.vue
@@ -2,38 +2,38 @@
   <nav class="navbar navbar-expand fixed-bottom bg-white border-top border-3 border-dark p-0">
     <div class="container-fluid justify-content-around">
       <ul class="navbar-nav text-center fw-bold">
-        <li class="nav-item">
-          <nuxt-link class="nav-link" to="/" :activeClass="activeClass">
-            <i class="bi bi-house-door fs-5"></i> <br>
-            <span class="smallest">Home</span>
-          </nuxt-link>
-        </li>
-        <li class="nav-item">
-          <nuxt-link class="nav-link" to="/iduka" :activeClass="activeClass">
+        <li class="nav-item me-3">
+          <nuxt-link class="nav-link  text-dark" to="/iduka" :activeClass="activeClass">
             <i class="bi bi-buildings fs-5"></i> <br>
             <span class="smallest">IDUKA</span>
           </nuxt-link>
         </li>
-        <li class="nav-item mx-4">
-          <nuxt-link class="nav-link" to="/elemen" :activeClass="activeClass">
+        <li class="nav-item me-3">
+          <nuxt-link class="nav-link  text-dark" to="/elemen" :activeClass="activeClass">
             <i class="bi bi-journal-bookmark-fill fs-5"></i> <br>
             <span class="smallest">Elemen</span>
           </nuxt-link>
         </li>
-        <li class="nav-item me-4">
-          <nuxt-link class="nav-link" to="/jurnal" :activeClass="activeClass">
+        <li class="nav-item me-3">
+          <nuxt-link class="nav-link text-dark" to="/" :activeClass="activeClass">
+            <i class="bi bi-house-door fs-5"></i> <br>
+            <span class="smallest">Home</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item me-3">
+          <nuxt-link class="nav-link  text-dark" to="/jurnal" :activeClass="activeClass">
             <i class="bi bi-journals fs-5"></i> <br>
             <span class="smallest">Jurnal</span>
           </nuxt-link>
         </li>
         <li class="nav-item">
-          <nuxt-link class="nav-link" href="/nilai" :activeClass="activeClass">
+          <nuxt-link class="nav-link  text-dark" href="/nilai" :activeClass="activeClass">
             <i class="bi bi-patch-check fs-5"></i> <br>
             <span class="smallest">Nilai</span>
           </nuxt-link>
         </li>
         <!-- <li class="nav-item">
-          <nuxt-link class="nav-link" href="/ubah-password">
+          <nuxt-link class="nav-link  text-dark" href="/ubah-password">
             <i class="bi bi-key fs-4"></i> <br>
             <span class="smallest">Password</span>
           </nuxt-link>
