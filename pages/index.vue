81dfb6aa25208164b1f3876bb3db6e389d62eeb5
hilmizul <netspytux@gmail.com>
2025-12-31 11:53:55 +0700

    minor update: experimental current mood user

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="card">
-    <div class="card-header">
+  <div>
+    <div class="card-header pb-3 ps-0 pe-0 text-center">
       <span v-if="isLoading" class="fst-italic text-muted">loading</span>
-      <span v-else-if="!isLoading && peserta" class="fw-bold">{{ peserta.nama.charAt(0).toUpperCase()+peserta.nama.toLowerCase().slice(1) }}</span>
+      <span v-else-if="!isLoading && peserta">
+        <span @click="() => isOpenEmojis = !isOpenEmojis" class="mood-container hand-cursor">
+          <span v-if="!currentMood" class="emoji">
+            <i class="bi bi-emoji-smile"></i><sup class="text-muted fw-normal"><i class="bi bi-plus"></i></sup>
+          </span>
+          <span v-else class="emoji">
+            {{ currentMood }}
+          </span>
+          <div v-if="isOpenEmojis" class="mood-item shadow-lg">
+            <div class="smallest p-2">Apa mood lu saat ini?</div>
+            <ul v-for="(emoji, i) in emojis" :key="i">
+              <li @click="handleMood(emoji)" class="hand-cursor">{{ emoji }}</li>
+            </ul>
+          </div>
+        </span>
+        <div class="fw-bold text-muted small">
+          <!--{{ peserta.nama.charAt(0).toUpperCase()+peserta.nama.toLowerCase().slice(1) }}-->
+          {{ peserta.nama }}
+        </div>
+      </span>
     </div>
+  </div>
+
+  <div class="card">
     <div class="card-body">
       <div class="row">
         <div class="col-md-12">
@@ -45,6 +67,7 @@
                 </div>
               </div>
             </div>
+
             <div class="row mb-3">
               <div class="col-lg-12">
                 <div class="text-muted mb-1">Temen satu tim:</div>
@@ -55,7 +78,7 @@
                     </tr>
                     <tr v-for="(p) in pemetaan" :key="p.id">
                       <td>
-                        <span class="fw-bold">{{ p.expand.siswa.nama }}</span> <br>
+                        <span class="fw-bold">{{ p.expand.siswa.currentMood }} {{ p.expand.siswa.nama }}</span> <br>
                         <span class="smallest text-muted">{{ p.expand.siswa.kelas }}</span>
                       </td>
                     </tr>
@@ -107,6 +130,9 @@ let pemetaan = ref([])
 let iduka = ref()
 let peserta = ref()
 let emptyPemetaan = ref(false)
+let isOpenEmojis = ref(false)
+let emojis = ref(["😃", "😥", "😔", "🤯", "🔥"])
+let currentMood = ref('')
 
 async function getCountJournal(loading=true) {
   isLoading.value = loading
@@ -156,9 +182,25 @@ async function getInfo(loading=true) {
   }
 }
 
+async function handleMood(emoji) {
+  currentMood.value = emoji
+  isOpenEmojis.value ? false : true
+  let res = await client.collection('siswa').update(user.user.value.siswa, {
+    "currentMood": currentMood.value
+  })
+}
+
+async function getCurrentMood() {
+  let res = await client.collection('siswa').getOne(user.user.value.siswa, {})
+  if(res) {
+    currentMood.value = res.currentMood
+  }
+}
+
 onMounted(() => {
   getCountJournal()
   getInfo()
+  getCurrentMood()
   client.autoCancellation(false)
   client.collection('pemetaan').subscribe('*', function(e){
     if(e.action == 'create' || e.action == 'update') getInfo(false)
@@ -172,5 +214,38 @@ onMounted(() => {
 <style scoped>
 .small, .small table {
   font-size: 14px !important;
+}
+.card .card-header {
+  font-size: 1.2rem;
+  background-color: transparent;
+  border-bottom: none !important;
+}
+ul {
+  padding: 0 7px 0 7px;
+  list-style-type: none;
+  display: inline-block !important;
+}
+ul li {
+  font-size: 1.5rem;
+}
+.mood-container {
+  position: relative;
+  padding-bottom: 0 !important;
+  padding-top: 5px;
+  display: block;
+  z-index: 1
+}
+.mood-item {
+  position: absolute; 
+  left: 40%;
+  bottom: -90px;
+  background-color: #fff;
+  border: 2px solid #000;
+}
+.emoji {
+  font-size: 3rem
+}
+.smallest {
+  font-size: 10pt
 }
 </style>
