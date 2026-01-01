8b80aa625f796fd9b07c9055b8042cd51b6f2301
hilmizul <netspytux@gmail.com>
2026-01-01 16:20:37 +0700

    fix kata baku: diperbaharui -> tersimpan

--- pages/iduka.vue
+++ pages/iduka.vue
@@ -19,25 +19,29 @@
           <form @submit.prevent="updateIduka">
             <div class="mb-4">
               <label for="pimpinan">Pimpinan <span class="text-danger">*</span></label>
-              <input v-model="form.pimpinan" :disabled="isLoading || isSaving" type="text" id="pimpinan" class="form form-control" placeholder="nama pimpinan/direktur" required>
+              <input v-model="form.pimpinan" :disabled="isLoading || isSaving" type="text" id="pimpinan"
+                class="form form-control" placeholder="nama pimpinan/direktur" required>
             </div>
             <div class="mb-4">
               <label for="kontak">Kontak <span class="text-danger">*</span></label>
-              <input v-model="form.kontak" :disabled="isLoading || isSaving" type="text" id="kontak" class="form form-control" placeholder="nomor telepon/HP kantor" required>
+              <input v-model="form.kontak" :disabled="isLoading || isSaving" type="text" id="kontak"
+                class="form form-control" placeholder="nomor telepon/HP kantor" required>
             </div>
             <div class="mb-4">
               <label for="email">Email (opsional)</label>
-              <input v-model="form.email" :disabled="isLoading || isSaving" type="email" id="email" class="form form-control" placeholder="email kantor kalo ada">
+              <input v-model="form.email" :disabled="isLoading || isSaving" type="email" id="email"
+                class="form form-control" placeholder="email kantor kalo ada">
             </div>
             <div class="mb-4">
               <label for="instruktur">Instruktur/Pembimbing <span class="text-danger">*</span></label>
-              <input v-model="form.pembimbing_iduka" :disabled="isLoading || isSaving" type="text" id="instruktur" class="form form-control" placeholder="nama pembimbing dikantor" required>
+              <input v-model="form.pembimbing_iduka" :disabled="isLoading || isSaving" type="text" id="instruktur"
+                class="form form-control" placeholder="nama pembimbing dikantor" required>
             </div>
             <button :disabled="isSaving" class="btn btn-info border border-2 border-dark">
               <span v-if="isSaving">Sedang menyimpan</span>
               <span v-else>Simpan</span>
             </button>
-            <span v-if="isSaved" class="ms-2 fst-italic text-muted fw-bold small">Berhasil diperbaharui!</span>
+            <span v-if="isSaved" class="ms-2 fst-italic text-muted fw-bold small">Berhasil tersimpan!</span>
           </form>
         </div>
       </div>
@@ -63,14 +67,14 @@ let form = ref({
 })
 let meta_pemetaan = ref()
 
-async function getIdukaByIdPeserta(loading=true) {
+async function getIdukaByIdPeserta(loading = true) {
   isLoading.value = loading
   client.autoCancellation(false)
   try {
     let res = await client.collection('pemetaan').getFirstListItem(`siswa="${peserta_id}"`, {
       expand: "iduka"
     })
-    if(res) {
+    if (res) {
       isLoading.value = false
       form.value = res.expand.iduka
       meta_pemetaan.value = res.expand.iduka
@@ -91,7 +95,7 @@ async function updateIduka() {
     "email": form.value.email,
     "pembimbing_iduka": form.value.pembimbing_iduka,
   })
-  if(res) {
+  if (res) {
     isSaving.value = false
     isSaved.value = true
   }
