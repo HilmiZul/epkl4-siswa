cb07576a6d5c9a4cbc0fb9f46ad96ee7501001ef
hilmizul <netspytux@gmail.com>
2026-04-28 21:04:42 +0700

    fix icon

--- components/Nav.vue
+++ components/Nav.vue
@@ -22,7 +22,7 @@
               <li class="list-group-item"><i class="bi bi-patch-check"></i> Nilai</li>
             </nuxt-link>
             <nuxt-link to="/proyek">
-              <li class="list-group-item"><i class="bi bi-proyek"></i> Proyek</li>
+              <li class="list-group-item"><i class="bi bi-easel"></i> Proyek</li>
             </nuxt-link>
             <nuxt-link to="/ubah-password">
               <li class="list-group-item"><i class="bi bi-key"></i> Ubah Password</li>
