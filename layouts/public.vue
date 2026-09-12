a89ed6ebef69383d51c82752fd158ba39f780719
hilmizul <netspytux@gmail.com>
2026-09-12 09:06:25 +0700

    decrease box-shadow

--- layouts/public.vue
+++ layouts/public.vue
@@ -34,7 +34,7 @@ html, body {
   border-radius: 32px !important;
   corner-shape: squircle;
   border: 2.5px solid #000000 !important;
-  box-shadow: 0.4rem 0.4rem 0 #000000 !important;
+  box-shadow: 0.2rem 0.2rem 0 #000000 !important;
 }
 .card .card-header {
   border-bottom: 2.5px solid #000 !important;
