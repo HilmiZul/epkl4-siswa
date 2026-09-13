1d2456bc285c26df4a449585d040d3a8204076e0
hilmizul <netspytux@gmail.com>
2026-09-13 13:54:10 +0700

    moving to app/ directory

--- components/LoadingPlaceholder.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <p v-for="(r,i) in props.row" :key="i" class="placeholder-glow">
-    <span :class="`placeholder col-${props.col} py-3`"></span>
-  </p>
-</template>
-
-<script setup>
-let props = defineProps(['col', 'row'])
-</script>
--- middleware/auth.ts
+++
@@ -1,8 +0,0 @@
-import { usePocketBaseClient } from "~/composables/pocketBaseClient"
-
-export default defineNuxtRouteMiddleware(async (to, _from) => {
-  const client = usePocketBaseClient()
-  const isUserLoggedIn = await client.authStore.isValid
-
-  if(!isUserLoggedIn) return navigateTo('/login')
-})
